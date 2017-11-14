f3563926f68046a8f8372b6229541454d0bc9c58
Sean <sean081694@gmail.com>
2017-11-14 15:30:34 -0700

    Created View
    Left to merge with everything

---
+++ Database/CreatedTheView.sql
@@ -0,0 +1,8 @@
+CREATE VIEW AllCharacterHouses
+AS
+SELECT Characters.*, Characters_Houses.*, Houses.*
+FROM Characters
+JOIN Characters_Houses
+ON Characters.Id = Characters_Houses.CharacterId
+JOIN Houses
+ON Characters_Houses.HouseId = Houses.Id
